45d59f8c3099cc325eb8c40555cf4c263332c58a
Joachim <joachimbulow@hotmail.dk>
2023-05-12 12:01:15 +0200

    refactor: move script files

--- scripts/delete-flink.sh
+++ scripts/delete-flink.sh
@@ -1,4 +1,4 @@
-# This make argo try to delete the managed resource, which will be possible once the job is gone
+# This makes argo try to delete the managed resource, which will be possible once the job is gone
 argocd app delete flink
 
 kubectl -n energy-balancing-mvp get jobs --no-headers=true -o custom-columns=:metadata.name | grep flink | xargs kubectl delete job
