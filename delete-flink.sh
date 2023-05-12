c070d0a128ff44bfb83e67cfd3af4ae978ce4dfd
Joachim <joachimbulow@hotmail.dk>
2023-05-12 11:05:25 +0200

    feat: update delete flink script

--- delete-flink.sh
+++ delete-flink.sh
@@ -1,5 +1,6 @@
+# This make argo try to delete the managed resource, which will be possible once the job is gone
+argocd app delete flink
+
 kubectl -n energy-balancing-mvp get jobs --no-headers=true -o custom-columns=:metadata.name | grep flink | xargs kubectl delete job
 
 kubectl -n energy-balancing-mvp get pods --no-headers=true -o custom-columns=:metadata.name | grep flink | xargs kubectl delete pod
-
-argocd app delete flink
