54751f6bccbd73a059bdb6a646ce614449e8aadd
Joachim <joachimbulow@hotmail.dk>
2023-05-15 15:46:09 +0200

    feat: scraping influx script

---
+++ scripts/scrape-influx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+time=$1
+filename=$2
+
+# Finding the battery actions
+
+result=$(kubectl exec charts-v1-influxdb-0 -- influx --database influx -precision rfc3339 --execute "SELECT sum(chargeActions) AS chargeSum, sum(dischargeActions) AS dischargeSum FROM actionSummary WHERE time > $time GROUP BY chargeActions, dischargeActions")
+chargeSum=$(echo $result | awk '/chargeActions/ {print $13}')
+dischargeSum=$(echo $result | awk '/dischargeActions/ {print $14}')
+
+
+# Finding the average frequency
+
+result=$(kubectl exec charts-v1-influxdb-0 -- influx --database influx -precision rfc3339 --execute "SELECT MEAN(frequency) AS averageFrequency FROM frequency WHERE time > $time")
+
+# - - - - Result is retarded, so this is the best we can do for now to extract the right data
+echo $result
+averageFrequency=$(echo $result | awk '/averageFrequency/ {print $8}')
+
+
+# Finding the max frequency
+
+result=$(kubectl exec charts-v1-influxdb-0 -- influx --database influx -precision rfc3339 --execute "SELECT MAX(frequency) AS maxFrequency FROM frequency WHERE time > $time")
+
+maxFrequency=$(echo $result | awk '/maxFrequency/ {print $8}')
+
+echo "Saving results to $filename..."
+echo "Average frequency: $averageFrequency"
+echo "Max frequency: $maxFrequency"
+echo "Charge actions: $chargeSum"
+echo "Discharge actions: $dischargeSum"
+echo -e "$time, $averageFrequency, $maxFrequency, $chargeSum, $dischargeSum" >> $filename.csv
