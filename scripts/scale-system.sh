8579bbb06ecc0b65a66cca8731b7a4817bfeae29
Joachim <joachimbulow@hotmail.dk>
2023-05-17 11:10:39 +0200

    feat: inline patch battery for script

--- scripts/scale-system.sh
+++ scripts/scale-system.sh
@@ -3,7 +3,7 @@
 BATTERY_PODS=$1
 N_BATTERIES=$2
 
-kubectl patch deployment battery -n energy-balancing-mvp --patch "$(cat ./scripts/patch-batteries.yaml)"
+kubectl patch deployment battery -n energy-balancing-mvp --patch "spec: { template: { spec: { containers: [ { name: "battery", env: [ { name: "N_BATTERIES", value: '${N_BATTERIES}' } ] } ] } } }"
 
 kubectl scale deployment battery --replicas=${BATTERY_PODS}
 
